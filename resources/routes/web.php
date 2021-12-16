f70602ee88c33e627c456bd0d1dab5f9b53a386b
Denis Ziberi <denis24951@gmail.com>
2021-12-16 18:30:40 +0100

    update main

--- resources/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Controllers\CalendarController;
-use App\Core\Router\Router;
-
-/** @var Router $router */
-$router->get("", controller: [CalendarController::class, 'index']);
-$router->get("new_route", controller: [CalendarController::class, 'new']);
